122d0881f81eb95dcd315311fcdf3a1964e66e7e
drikavonis <ozorio.adrielly@escola.pr.gov.br>
2026-06-30 09:34:53 -0300

    jj

--- main.js
+++ main.js
@@ -16,8 +16,11 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2026-11-30");
+const tempoObjetivo2 = new Date("2026-11-30");
+const tempoObjetivo3 = new Date("2026-11-30");
+const tempoObjetivo4 = new Date("2026-11-30");
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
 function calculaTempo(tempoObjetivo) {
   let tempoAtual = new Date();
